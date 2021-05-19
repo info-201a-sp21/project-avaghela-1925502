a42bd6f13e5a8dca238629814a56a575f2db27af
Arjun Vaghela <Arjun@MacBook-Pro-2.home>
2021-05-18 23:57:56 -0700

    Arjun Vaghela popularity by year

---
+++ scripts/popularity_by_year.R
@@ -0,0 +1,21 @@
+library("dplyr")
+library("leaflet")
+library("ggplot2")
+library("plotly")
+library("lintr")
+library("tidyverse")
+
+data_by_year_long <- read.csv("/Users/Arjun/Desktop/Info 201/project-avaghela-1925502/data/Spotify/data_by_year_o.csv")
+
+data_by_year <- data_by_year_long %>% 
+  select(year, popularity) 
+
+x <- data_by_year$year
+y <- data_by_year$popularity
+
+ggplot(data_by_year, aes(x, y)) +
+  geom_point() +
+  theme_minimal() +
+  xlab("Year") +
+  ylab ("Popularity") +
+  geom_smooth(method=lm, se=FALSE)
