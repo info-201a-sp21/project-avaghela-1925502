70c5623fe1d113e8177f297d795be1dc5df16a5c
gosha <ghyppa@uw.edu>
2021-05-19 21:31:12 -0700

    Summary Table

--- scripts/summary_table.R
+++ scripts/summary_table.R
@@ -8,50 +8,53 @@ library(forestmangr)
 library(stringr)
 data_by_year <- read.csv("data/Spotify/data_by_year_o.csv")
 
-table_summary <- function(df){
-df <- data_by_year %>%
-  mutate(
-    decade = floor(year / 10) * 10,
-    duration_minutes = round(duration_ms / 60000, 2),
-    acousticness_pct = acousticness * 100,
-    danceability_pct = danceability * 100,
-    energy_pct = energy * 100,
-    instrumentalness_pct = instrumentalness * 100,
-    liveness_pct = liveness * 100
-  ) %>%
-  group_by(decade) %>%
-  summarise_all(mean) %>%
-  mutate(
-    key = round(key),
-    tempo = round(tempo)
-  ) %>%
-  select(
-    decade, acousticness_pct, danceability_pct, energy_pct,
-    instrumentalness_pct, liveness_pct, loudness, tempo, key,
-    duration_minutes
-  )
+table_summary <- function(df) {
+  df <- data_by_year %>%
+    mutate(
+      decade = floor(year / 10) * 10, # convert year to decade
+      duration_minutes = round(duration_ms / 60000, 2),   # ms to minutes
+      acousticness_pct = acousticness * 100,
+      danceability_pct = danceability * 100,
+      energy_pct = energy * 100,
+      instrumentalness_pct = instrumentalness * 100,
+      liveness_pct = liveness * 100
+    ) %>%
+    group_by(decade) %>%
+    summarise_all(mean) %>%
+    # taking the average of all values for each decade
+    mutate(
+      key = round(key),
+      tempo = round(tempo)
+    ) %>%
+    select(
+      decade, acousticness_pct, danceability_pct, energy_pct,
+      instrumentalness_pct, liveness_pct, loudness, tempo, key,
+      duration_minutes
+    )
 
-df <- round_df(df, 1)
+  df <- round_df(df, 1)
 
-df <- df %>%
-  rename(
-    Decade = decade,
-    Acousticness = acousticness_pct,
-    Danceability = danceability_pct,
-    Energy = energy_pct,
-    Instrumentalness = instrumentalness_pct,
-    Liveness = liveness_pct,
-    "Loudness (dB)" = loudness,
-    "Tempo (BPM)" = tempo,
-    Key = key,
-    "Duration (minutes)" = duration_minutes
-  ) %>% 
-  mutate(
-    Acousticness = paste0(Acousticness, "%"),
-    Danceability = paste0(Danceability, "%"),
-    Energy = paste0(Energy, "%"),
-    Instrumentalness = paste0(Instrumentalness, "%"),
-    Liveness = paste0(Liveness, "%")
-  ) 
-return(df)
+  df <- df %>%
+    # Labeling columns
+    rename(
+      Decade = decade,
+      Acousticness = acousticness_pct,
+      Danceability = danceability_pct,
+      Energy = energy_pct,
+      Instrumentalness = instrumentalness_pct,
+      Liveness = liveness_pct,
+      "Loudness (dB)" = loudness,
+      "Tempo (BPM)" = tempo,
+      Key = key,
+      "Duration (minutes)" = duration_minutes
+    ) %>%
+    # Adding percent symbols to columns
+    mutate(
+      Acousticness = paste0(Acousticness, "%"),
+      Danceability = paste0(Danceability, "%"),
+      Energy = paste0(Energy, "%"),
+      Instrumentalness = paste0(Instrumentalness, "%"),
+      Liveness = paste0(Liveness, "%")
+    )
+  return(df)
 }
