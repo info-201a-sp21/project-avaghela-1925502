50d75d1c009a5a04fb678e5131802527924e4199
shrlgh8381 <96kiho@gmail.com>
2021-05-19 19:59:45 -0700

    test uploading

--- scripts/loudness.R
+++
@@ -1,3 +0,0 @@
-
-spotify <- read.csv("data/Spotify/data_by_year_o.csv", stringsAsFactors = FALSE)
-
---
+++ scripts/loudness_by_year.R
@@ -0,0 +1,20 @@
+# Import library
+library("ggplot2")
+
+# Function that plots a line graph to visualize how loudness of
+# music changed over time.
+plot_loudness_year <- function(df) {
+  ggplotly(ggplot(data = df, aes(x = year, y = loudness)) + 
+    geom_line() +
+    geom_smooth() +
+    ggtitle("The Trend of Loudness of Music Over Time") +
+    labs(y = "Loudness (dB)", x = "Year"))
+}
+
+test = read.csv("data/Spotify/data_by_year_o.csv")
+
+min_year <- min(test$year)
+
+max_year <- max(test$year)
+
+max(test$loudness)
