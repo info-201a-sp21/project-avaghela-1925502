a9f011e3f65ae59bd7a3cb56c1cb3213e6a76ffb
shrlgh8381 <96kiho@gmail.com>
2021-05-20 20:10:44 -0700

    ReRan styler and lint for error

--- scripts/dance_instrumental.R
+++ scripts/dance_instrumental.R
@@ -3,20 +3,22 @@
 library("dplyr")
 library("ggplot2")
 library("tidyr")
+library("lintr")
+library("styler")
 
 # Stacked bar chart
 inst_dance_chart <- function(data) {
-  data_by_year_long <- data %>% 
+  data_by_year_long <- data %>%
     select(year, danceability, instrumentalness) %>% # select relevant columns
     gather(variable, measure, -year) # Reshaped dataframe so there's only 1 fill variable.
-  
+
   chart <- ggplot(data_by_year_long) +
     geom_col(
       mapping = aes(x = year, y = measure, fill = variable) # Create chart
     ) +
     labs(
       title = "Danceability and Instrumentalness From 1921 to 2020",
-      y = "Measure" # Renamed title and y-axis label
+      y = "Danceability and Instrumentalness Index (0.0 to 1.0" # Renamed title and y-axis label
     ) +
     scale_x_continuous(
       name = "Year",
@@ -27,6 +29,6 @@ inst_dance_chart <- function(data) {
       labels = c("Danceability", "Instrumentalness"),
       values = c("#80B1D3", "#FDB462") # Renamed legend labels and changed colors
     )
-  
+
   return(chart)
 }
--- scripts/loudness_by_year.R
+++ scripts/loudness_by_year.R
@@ -1,14 +1,13 @@
 # Import library
 library("ggplot2")
+library("lintr")
 
 # Function that plots a line graph to visualize how loudness of
 # music changed over time.
 plot_loudness_year <- function(df) {
-  ggplotly(ggplot(data = df, aes(x = year, y = loudness)) + 
+  ggplotly(ggplot(data = df, aes(x = year, y = loudness)) +
     geom_line() +
     geom_smooth() +
     ggtitle("The Trend of Loudness of Music Over Time") +
     labs(y = "Loudness (dB)", x = "Year"))
 }
-
-
