05ef4a6e1fd5cadb8618991c3c2fff263c9e5ddc
shrlgh8381 <96kiho@gmail.com>
2021-06-03 20:04:34 -0700

    final revision on the uifile

--- scripts/app_ui.R
+++ scripts/app_ui.R
@@ -11,16 +11,19 @@ tab_overview <- sidebarLayout(
     tags$div(
       id = "overview_paragrah",
       p(
-        "As a group we decided to explore the development of music throughout the past century. The reason why we decided
-      to do this was that we all had an interest in this field. Collectively we wanted to address key questions
-      relating to how music has evolved over time such as, popularity, loudness, and
-      danceability/instrumenalness. Using a ",
+        "As a group we decided to explore the development of music throughout
+        the past century. The reason why we decided to do this was that we all
+        had an interest in this field. Collectively we wanted to address key
+        questions relating to how music has evolved over time such as,
+        popularity, loudness, and danceability/instrumenalness. Using a ",
         a(
-          href = "https://www.kaggle.com/yamaerenay/spotify-dataset-19212020-160k-tracks?select=data_by_year_o.csv",
+          href = "https://www.kaggle.com/yamaerenay/spotify-dataset-19212020-
+          160k-tracks?select=data_by_year_o.csv",
           "Spotify dataset"
         ),
-        "aggregated by Yamac Eren Ay, we were able to curate visualizations to make interpreting the evolution of music
-      easier."
+        "aggregated by Yamac Eren Ay, we were able to curate visualizations to
+         make interpreting the evolution of music
+         easier."
       )
     )
   ),
@@ -58,14 +61,19 @@ tab_1 <- sidebarLayout(
     ),
     tags$div(
       id = "plot_1_message",
-      "  Plot 1 is a scatter plot graph that shows the trend of the popularity of music over the years. This graphs reveals how
-       music has changed along with our society with both increasing and decreasing levels of popularity. The plot includes a slider
-       in which you can change what year range you are viewing for and this allows for more specific years of music to be focused on
-       to find trends. The points on the plot are also interactive, showing both the year and popularity level when hovered over.",
+      "  Plot 1 is a scatter plot graph that shows the trend of the popularity
+       of music over the years. This graphs reveals how music has changed along
+       with our society with both increasing and decreasing levels of
+       popularity. The plot includes a slider in which you can change what
+       year range you are viewing for and this allows for more specific years
+       of music to be focused on to find trends. The points on the plot are
+       also interactive, showing both the year and popularity
+       level when hovered over.",
       br(),
       br(),
-      "   This plot successfully answers and demonstrates the question of how has the popularity of music changed over time by showing
-       the relationship between music popularity and years.
+      "   This plot successfully answers and demonstrates the question of how
+      has the popularity of music changed over time by showing
+      the relationship between music popularity and years.
        "
     )
   )
@@ -91,11 +99,11 @@ tab_2 <- sidebarLayout(
       id = "plot_2_message",
       "This bar chart is designed to compare the changes in overall danceability
       of music with the changes in overall instrumentalness of music over time,
-      in order to determine whether or not there exist a relationship between the two.
-      Observations from this chart reveals that from 1921 to 2020, there seems
-      to be a downward trend in instrumentalness of music, while danceability
-      remained relatively constant. This indicates that there might be no
-      relationship between instrumentalness and danceability."
+      in order to determine whether or not there exist a relationship between
+      the two.Observations from this chart reveals that from 1921 to 2020,
+      there seems to be a downward trend in instrumentalness of music,
+      while danceability remained relatively constant. This indicates that
+      there might be no relationship between instrumentalness and danceability."
     )
   )
 )
@@ -121,17 +129,19 @@ tab_3 <- sidebarLayout(
     ),
     tags$div(
       id = "plot3_message",
-      "The chart below visualizes how the loudness feature (dB) of music has changed
-            over time. Each plot captures a specific loudness level of that year, the lines
-            that connect each plot represent an increase or a decrease of that level in between
-            the years, and the smooth line visualizes the overall trend of the loudness level
-            over time from years 1921 to 2020.",
+      "The chart below visualizes how the loudness feature (dB) of music has
+      changed over time. Each plot captures a specific loudness level of that
+      year, the lines that connect each plot represent an increase or a
+      decrease of that level in between the years, and the smooth line
+      visualizes the overall trend of the loudness level over time from
+      years 1921 to 2020.",
       br(),
       br(),
-      "Users can interact with the graph by changing the input year on the slide bar widget
-            , which will allow them to observe the change in loudness level during the specified year range.
-            This graph helps answer how the audio feature, loudness level specifically, has developed
-            over time in accordance with change in music trend and peoples' preferences"
+      "Users can interact with the graph by changing the input year on the
+      slide bar widget, which will allow them to observe the change in loudness
+      level during the specified year range. This graph helps answer how the
+      audio feature, loudness level specifically, has developed over time in
+      accordance with change in music trend and peoples' preferences"
     )
   )
 )
@@ -141,13 +151,13 @@ tab_4_popularity <- sidebarLayout(
   # h2("Music popularity insights from 1921 - 2021:"),
   sidebarPanel(
     tags$div(
-      "This table displays the positive popularity trend throughout the 11 decades
-      of data collected. This trend is beneficial in answering one of the initial
-      questions our research was intended to answer. The broader implications
-      of this insight are the affects on the music industry in the future.
-      Interestingly there is a large jump in popularity from 1940-1950 which
-      increases steadily as music technology becomes more accessable to the
-      public."
+      "This table displays the positive popularity trend throughout the
+      11 decades of data collected. This trend is beneficial in answering
+      one of the initial questions our research was intended to answer.
+      The broader implications of this insight are the affects on the music
+      industry in the future. Interestingly there is a large jump in popularity
+      from 1940-1950 which increases steadily as music technology becomes
+      more accessable to the public."
     )
   ),
   mainPanel(
